a18e335c45560387c99b7b63739360381e1e4226
M Bilal Hadid <110343359+MariaFajruddin1@users.noreply.github.com>
2023-12-17 23:49:32 +0500

    Maria changes

--- src/app/components/shared/Hero.tsx
+++ src/app/components/shared/Hero.tsx
@@ -11,7 +11,7 @@ import "swiper/css";
 import "swiper/css/navigation";
 
 // import required modules
-import { Navigation } from "swiper/modules";
+import { Navigation, Pagination } from "swiper/modules";
 // import Image from "next/image";
 import Symbol from "/public/images/symbol.png";
 import JL from "/public/images/cl1.jpg";
@@ -26,30 +26,45 @@ import BlogSlider from "../shared/BlogSlider"
 import mainnew from '../../../../public/images/mainnew.png'
 import worknew from '../../../../public/images/worknew.png'
 import girl from '../../../../public/images/girl.png'
+import Hero01 from '../../../../public/images/Hero01.png'
+import Hero02 from '../../../../public/images/Hero02.png'
+import Hero03 from '../../../../public/images/Fitsense - Gym and Fitness HTML Template_files/2.png'
 
 const Hero = () => {
   return (
     <>
-      <Swiper navigation={true} modules={[Navigation]} className="mySwiper bg-[url('/images/bg.png')] h-auto py-10">
+      <Swiper pagination={{
+        dynamicBullets: true,
+      }}
+        modules={[Pagination]} className="mySwiper bg-[url('/images/bg.png')] h-auto py-10">
         <SwiperSlide>
           <div>
-            <div className="h-screen flex  justify-center items-center pt-28">
-              <Image alt="equip" height={800} width={800} src={mainnew} />
-              
+            <div className="h-screen flex justify-center gap-10 items-center pt-28">
+              <div className="align-start">
+                <p className="text-white text-2xl"><span className="text-[#FAFF00]">|</span> ARE YOU READY TO</p>
+                <h1 className="text-7xl font-semibold text-white"><span className="text-[#FAFF00]">GET FIT</span>, STRONG<br />& MOTIVATED!</h1>
+              </div>
+              <Image alt="equip" height={450} width={450} src={Hero01} />
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div>
-            <div className="h-screen flex  justify-center items-center pt-36 pb-10">
-              <Image alt="equip" height={600} width={600} src={worknew} />
+            <div className="h-screen flex items-center">
+              <Image alt="equip" height={900} width={900} src={Hero02} className="mt-32" />
+              <div className="">
+                <h1 className="text-6xl font-semibold text-white text-8xl">BUILD <span className="text-[#FAFF00]"><br />YOUR BODY</span></h1>
+              </div>
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div>
             <div className="h-screen flex  justify-center items-center pt-24">
-              <Image alt="equip" height={700} width={700} src={girl} />
+              <div className="align-start">
+                <h1 className="text-9xl font-semibold text-white"><span className="text-[#FAFF00]">WORKOUT</span><br />WITH ME</h1>
+              </div>
+              <Image alt="equip" height={600} width={600} src={Hero03} />
             </div>
           </div>
         </SwiperSlide>
